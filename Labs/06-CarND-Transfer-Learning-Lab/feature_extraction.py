81ea898af5a80bdfc87f431bd05d93f43db71699
Raghavendra Manjegowda <raghavendrahm0410@gmail.com>
2017-01-10 22:52:44 -0800

    adding Transfer Learning Lab

---
+++ Labs/06-CarND-Transfer-Learning-Lab/feature_extraction.py
@@ -0,0 +1,71 @@
+import pickle
+import tensorflow as tf
+
+# TODO: import Keras layers you need here
+import numpy as np
+from keras.layers import Input, Flatten, Dense
+from keras.models import Model
+
+epochs = 10
+batch_size = 128
+
+flags = tf.app.flags
+FLAGS = flags.FLAGS
+
+# command line flags
+flags.DEFINE_string('training_file', '', "Bottleneck features training file (.p)")
+flags.DEFINE_string('validation_file', '', "Bottleneck features validation file (.p)")
+
+
+def load_bottleneck_data(training_file, validation_file):
+    """
+    Utility function to load bottleneck features.
+
+    Arguments:
+        training_file - String
+        validation_file - String
+    """
+    print("Training file", training_file)
+    print("Validation file", validation_file)
+
+    with open(training_file, 'rb') as f:
+        train_data = pickle.load(f)
+    with open(validation_file, 'rb') as f:
+        validation_data = pickle.load(f)
+
+    X_train = train_data['features']
+    y_train = train_data['labels']
+    X_val = validation_data['features']
+    y_val = validation_data['labels']
+
+    return X_train, y_train, X_val, y_val
+
+
+def main(_):
+    # load bottleneck data
+    X_train, y_train, X_val, y_val = load_bottleneck_data(FLAGS.training_file, FLAGS.validation_file)
+
+    print(X_train.shape, y_train.shape)
+    print(X_val.shape, y_val.shape)
+
+    # TODO: define your model and hyperparams here
+    # make sure to adjust the number of classes based on
+    # the dataset
+    # 10 for cifar10
+    # 43 for traffic
+    nb_classes = len(np.unique(y_train))
+
+    # define model
+    input_shape = X_train.shape[1:]
+    inp = Input(shape=input_shape)
+    x = Flatten()(inp)
+    x = Dense(nb_classes, activation='softmax')(x)
+    model = Model(inp, x)
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+    # TODO: train your model here
+    history = model.fit(X_train, y_train, nb_epoch=epochs, batch_size=batch_size, validation_data=(X_val, y_val), shuffle=True)
+
+# parses flags and calls the `main` function above
+if __name__ == '__main__':
+    tf.app.run()
