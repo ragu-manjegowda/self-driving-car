2fb8ca4235bbd879aa60b29703788c279b174344
Ragu Manjegowda <raghavendrahm0410@gmail.com>
2017-06-02 10:27:00 -0700

    style: Format MPC.cpp and MPC.h files
    Moved the global vriables and constant from cpp to header file.
    This commit doesn't has any logic implementation, just the formating.
    
    Signed-off-by: Ragu Manjegowda <raghavendrahm0410@gmail.com>

--- src/MPC.h
+++ src/MPC.h
@@ -3,18 +3,69 @@
 
 #include <vector>
 #include "Eigen-3.3/Eigen/Core"
+#include <cppad/cppad.hpp>
+#include <cppad/ipopt/solve.hpp>
 
 using namespace std;
 
+// We set the number of timesteps to 25
+// and the timestep evaluation frequency or evaluation
+// period to 0.05.
+size_t N = 25;
+double dt = 0.05;
+
+// This value assumes the model presented in the classroom is used.
+//
+// It was obtained by measuring the radius formed by running the vehicle in the
+// simulator around in a circle with a constant steering angle and velocity on a
+// flat terrain.
+//
+// Lf was tuned until the the radius formed by the simulating the model
+// presented in the classroom matched the previous radius.
+//
+// This is the length from front to CoG that has a similar radius.
+const double Lf = 2.67;
+
+// Both the reference cross track and orientation errors are 0.
+// The reference velocity is set to 40 mph.
+double ref_cte = 0;
+double ref_epsi = 0;
+double ref_v = 40;
+
+// The solver takes all the state variables and actuator
+// variables in a singular vector. Thus, we should to establish
+// when one variable starts and another ends to make our lifes easier.
+size_t x_start = 0;
+size_t y_start = x_start + N;
+size_t psi_start = y_start + N;
+size_t v_start = psi_start + N;
+size_t cte_start = v_start + N;
+size_t epsi_start = cte_start + N;
+size_t delta_start = epsi_start + N;
+size_t a_start = delta_start + N - 1;
+
+typedef CPPAD_TESTVECTOR(double) Dvector;
+
+// TODO: Set the number of model variables (includes both states and inputs).
+// For example: If the state is a 4 element vector, the actuators is a 2
+// element vector and there are 10 timesteps. The number of variables is:
+//
+// 4 * 10 + 2 * 9
+size_t n_vars = N * 6 + (N - 1) * 2;
+
+// TODO: Set the number of constraints
+size_t n_constraints = N * 6;
+
 class MPC {
- public:
-  MPC();
+	
+	public:
+		MPC();
 
-  virtual ~MPC();
+		virtual ~MPC();
 
-  // Solve the model given an initial state and polynomial coefficients.
-  // Return the first actuatotions.
-  vector<double> Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs);
+		// Solve the model given an initial state and polynomial coefficients.
+		// Return the first actuatotions.
+		vector<double> Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs);
 };
 
 #endif /* MPC_H */
