a383367633cdd61812136120edec7442c3fe9f44
Raghavendra Manjegowda <raghavendrahm0410@gmail.com>
2017-06-03 00:01:16 -0700

    Revert "style: Format MPC.cpp and MPC.h files"
    This reverts commit 2fb8ca4235bbd879aa60b29703788c279b174344.

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -5,6 +5,44 @@
 
 using CppAD::AD;
 
+// TODO: Set the timestep length and duration
+
+// We set the number of timesteps to 25
+// and the timestep evaluation frequency or evaluation
+// period to 0.05.
+size_t N = 25;
+double dt = 0.05;
+
+// This value assumes the model presented in the classroom is used.
+//
+// It was obtained by measuring the radius formed by running the vehicle in the
+// simulator around in a circle with a constant steering angle and velocity on a
+// flat terrain.
+//
+// Lf was tuned until the the radius formed by the simulating the model
+// presented in the classroom matched the previous radius.
+//
+// This is the length from front to CoG that has a similar radius.
+const double Lf = 2.67;
+
+// Both the reference cross track and orientation errors are 0.
+// The reference velocity is set to 40 mph.
+double ref_cte = 0;
+double ref_epsi = 0;
+double ref_v = 40;
+
+// The solver takes all the state variables and actuator
+// variables in a singular vector. Thus, we should to establish
+// when one variable starts and another ends to make our lifes easier.
+size_t x_start = 0;
+size_t y_start = x_start + N;
+size_t psi_start = y_start + N;
+size_t v_start = psi_start + N;
+size_t cte_start = v_start + N;
+size_t epsi_start = cte_start + N;
+size_t delta_start = epsi_start + N;
+size_t a_start = delta_start + N - 1;
+
 class FG_eval {
  public:
   // Fitted polynomial coefficients
@@ -114,7 +152,8 @@ MPC::~MPC() {}
 vector<double> MPC::Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs) {
   bool ok = true;
   size_t i;
-  
+  typedef CPPAD_TESTVECTOR(double) Dvector;
+
   double x = state[0];
   double y = state[1];
   double psi = state[2];
@@ -122,7 +161,14 @@ vector<double> MPC::Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs) {
   double cte = state[4];
   double epsi = state[5];
 
-  
+  // TODO: Set the number of model variables (includes both states and inputs).
+  // For example: If the state is a 4 element vector, the actuators is a 2
+  // element vector and there are 10 timesteps. The number of variables is:
+  //
+  // 4 * 10 + 2 * 9
+  size_t n_vars = N * 6 + (N - 1) * 2;
+  // TODO: Set the number of constraints
+  size_t n_constraints = N * 6;
 
   // Initial value of the independent variables.
   // SHOULD BE 0 besides initial state.
